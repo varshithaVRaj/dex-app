b70bd2abfbc97d3e1aac5ccc8e2d56f9563e6182
varshitha v <varshitha.vraj@winds.co.in>
2025-08-31 20:03:49 +0530

    pushing initial files

---
+++ Dex/Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Varshitha VRaj on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
