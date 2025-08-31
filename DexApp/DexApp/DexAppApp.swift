e4477fc8a29786d5ec413c2aa93b60fb58b4966f
varshitha v <varshitha.vraj@winds.co.in>
2025-08-31 19:53:54 +0530

    pushing initial files

---
+++ DexApp/DexApp/DexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DexAppApp.swift
+//  DexApp
+//
+//  Created by Varshitha VRaj on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
